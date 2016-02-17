d3e438de65e0bb1397905f13eb87cc5b6b0b9ad2
Arthur Pankiewicz <arthurpankiewicz@gmail.com>
2016-02-17 19:35:17 +0000

    seeding and choice controller

---
+++ DiplomaDataModel/OptionPicker/OptionPickerContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DiplomaDataModel
+{
+    public class OptionPickerContext : DbContext
+    {
+        public OptionPickerContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Option> Options { get; set; }
+        public DbSet<YearTerm> YearTerms { get; set; }
+        public DbSet<Choice> Choices { get; set; }
+    }
+}
